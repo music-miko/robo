a11e89e66fcfb3d68cbbbeeb7547268deda1f00e
music-miko <voilet856@gmail.com>
2025-06-06 03:22:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -85,7 +85,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/1557a544d9b4f051f99c7-57891fa1d7578f5b79.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/lt563a.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph/file/9f9bde8939034b95f3e59-a0d7a8216c1faccd19.jpg"
