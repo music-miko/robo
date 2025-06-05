729a480611857fb5d2f912cefb6dc22026444cc7
music-miko <voilet856@gmail.com>
2025-06-06 03:12:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/DeadlineTechTeam")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Cloning_Music_Bot")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DeadlineTechsupport")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
