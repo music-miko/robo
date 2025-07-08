99e399011f8bbab0de362fcbbc8c9bc38590694e
music-miko <voilet856@gmail.com>
2025-07-08 20:13:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 #Get API_KEY from @DeadlineTechOwner or @DeadlineApiBot
-API_BASE_URL = getenv("API_BASE_URL", "https://deadlineTech.site")
+API_BASE_URL = getenv("API_URL", "https://deadlineTech.site")
 API_KEY = getenv("API_KEY")
 
 # Get your mongo url from cloud.mongodb.com
@@ -84,6 +84,14 @@ votemode = {}
 autoclean = []
 confirmer = {}
 
+# YouTube Api
+RequestApi = 0
+downloadedApi = 0
+failedApi = 0
+failedApiLinkExtract = 0
+ReqYt = 0
+DlYt = 0
+FailedYt = 0
 
 START_IMG_URL = getenv(
     "START_IMG_URL", "https://files.catbox.moe/fzpvz0.jpg"
